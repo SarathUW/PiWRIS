a24bddec80260a4fb449c66ee75b88d59f445004
Ishaan Ambrish <ishaan.ambrish@gmail.com>
2024-11-12 15:46:36 -0800

    ran into issues with webdriver; added some links from devtools

--- docs/web_scraping.py
+++ docs/web_scraping.py
@@ -1,2 +1,9 @@
+# import necessary packages
 import pandas as pd
 from selenium import webdriver
+
+# set browser
+driver = webdriver.Edge()
+
+# get rainfall data
+driver.get("https://wdo.indiawris.gov.in/api/comm/src/rainfall")
