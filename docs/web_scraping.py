51188f5e65d3d60181821d767d76fff00720a865
Ishaan Ambrish <ishaan.ambrish@gmail.com>
2024-11-18 12:45:32 -0800

    tried some different ways to get the driver to work with natalie in office hours yesterday (none of them work for now)

--- docs/web_scraping.py
+++ docs/web_scraping.py
@@ -2,16 +2,50 @@
 
 ## import necessary packages
 import pandas as pd
+import subprocess as sp
 from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+# from selenium.webdriver.common.keys import keys
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+#from selenium.common.exceptions import TimeoutException
+
+# driver code
+
+## create service object
+#edgeService = Service(
+#   "mnt/c/Users/ishaa/Downloads/edgedriver_win64/msedgedriver" 
+#)
+
+## create webdriver object
+#edgeDriver = webdriver.Edge(service = edgeService)
+
+# testing
+#System.setProperty("webdriver.edge.driver", "C:\Users\ishaa\Downloads\edgedriver_win64\msedgedriver.exe")
 
 # set browser
-driver = webdriver.Edge()
+#driver = webdriver.Edge(executable_path = "C:\\Users\\ishaa\\Downloads\\edgedriver_win64\\msedgedriver.exe")
+
+#driver = webdriver.Edge(service = Service(EdgeChromiumDriverManager().install(), service_args = ['--log-level=DEBUG'], log_output = sp.STDOUT))
+#service = webdriver.EdgeService(EdgeChromiumDriverManager().install())
+service = webdriver.EdgeService()
+
+driver = webdriver.Edge(service=service)
 
 # get data links from WRIS using devtools
 
 ## rainfall
 ### location
-# driver.get("https://wdo.indiawris.gov.in/api/comm/src/rainfall")
+driver.get("http://www.python.org")
+#timeout = 3
+#try:
+#    element_present = EC.presence_of_element_located((By.ID, 'main'))
+#    WebDriverWait(driver, timeout).until(element_present)
+#except TimeoutException:
+#    print("Timed out waiting for page to load")
+#finally:
+#    print("Page loaded")
+driver.quit()
+#driver.get("https://wdo.indiawris.gov.in/api/comm/src/rainfall")
 
 ### table
 #https://wdo.indiawris.gov.in/api/rf/rfTable
