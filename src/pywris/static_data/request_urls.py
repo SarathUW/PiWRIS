3765546b1909ba9ad8d4285131ec89d423a08ded
shahzaib1007 <khan.shahzaib1007@gmail.com>
2024-12-07 21:13:14 -0800

    Added get_reservoir_info that fetches reservoir static attrs mainly lat and lon (By Shahzaib and Sanchit )

--- src/pywris/static_data/request_urls.py
+++ src/pywris/static_data/request_urls.py
@@ -15,6 +15,11 @@ requests_config = {
             'payload': {"stnVal":{"Reporttype":"Level & Storage Timeseries","View":"Admin","Agencyname":"All",
                               "Reservoir":"\"{}\"","Timestep":"{}","Parent":"","Child":"","Startdate":"{}","Enddate":"{}"}},
             'method': 'POST'
+        },
+        'get_reservoir_info':{
+            'url':'https://arc.indiawris.gov.in/server/rest/services/NWIC/Reservoir_Points/MapServer/0/query?',
+            'payload':'f=json&outFields=*&returnGeometry=false&spatialRel=esriSpatialRelIntersects&where=station_type=%27Reservoir%27%20AND%20station_name%20IN%20({})',
+            'method': 'GET'
         }
     },
     'geounits': {
@@ -22,6 +27,7 @@ requests_config = {
             'url': 'https://arc.indiawris.gov.in/server/rest/services/Admin/Administrative_NWIC/MapServer/1/query?',
             'payload': 'f=json&orderByFields=district&outFields=*&returnGeometry=false&spatialRel=esriSpatialRelIntersects&where=state%20in%20(%27{}%27)',
             'method': 'GET'
-        },
-    }
+        }
+    },
+
 }
