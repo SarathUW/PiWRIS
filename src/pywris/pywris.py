2a5b223733d79851bf67dd121a1407d6098d4dd4
SarathUW <saraths96@gmail.com>
2024-12-10 16:36:01 -0800

    "Added rich hypertext for PyWris HydroFrame for Jupyter Environments"

--- src/pywris/pywris.py
+++ src/pywris/pywris.py
@@ -1,10 +1,11 @@
 import pandas as pd
-from IPython.display import display, HTML
+# from IPython.display import display, HTML
 
 import pywris.geo_units.components as geo_components
 import pywris.surface_water.storage.reservoir as py_reservoir
 
 class HydroFrame:
+
     def __init__(self, states=None, basins=None, reservoirs=None):
         self.states = {}
         self.basins = {}
@@ -23,14 +24,20 @@ class HydroFrame:
             for res in reservoirs:
                 self.add_reservoir(res, 'None')
     
-    def add_state(self, state_name):        
-        self.states[state_name] = geo_components.State(state_name)
+    def add_state(self, state_names):
+        
+        if isinstance(state_names, list):
+            geo_components.check_valid_states(state_names)
+            for state in state_names:
+                self.states[state] = geo_components.State(state)
+        else:
+            self.states[state_names] = geo_components.State(state_names)
         
     def add_reservoir(self, reservoir_name, state_name):
         if reservoir_name not in self.reservoirs:
             self.reservoirs[reservoir_name] = geo_components.Reservoir(reservoir_name, state_name)
             
-    def fetch_reservoirs(self, end_date, start_date='1991-01-01', timestep='Daily'):
+    def fetch_reservoir_data(self, end_date, start_date='1991-01-01', timestep='Daily'):
         if self.states.keys():
             self.reservoirs, self.reservoir_df = py_reservoir.get_reservoirs(end_date, start_date, timestep, selected_states=list(self.states.keys()))
         elif self.basins.keys():
@@ -41,66 +48,63 @@ class HydroFrame:
     def filter(self, on, by, range=None, values=None):
         return filter(self, on, by, range, values)
     
-    def _generate_display_data(self):
-        """ Prepare the data to be displayed in a neat format """
-        display_data = {
-            "States": list(self.states.keys()),
-            "Reservoirs": list(self.reservoirs.keys()),
-        }
-        return display_data
-
-    def _expand_reservoir(self, reservoir_name):
-        """ Expands to show attributes of a reservoir """
-        reservoir = self.reservoirs.get(reservoir_name)
-        if reservoir:
-            return {
-                "Reservoir Name": reservoir.reservoir_name,
-                "State": reservoir.state.state_name,
-                "District": reservoir.district.district_name if reservoir.district else None,
-                "Latitude": reservoir.latitude,
-                "Longitude": reservoir.longitude,
-                "Block Name": reservoir.block_name,
-                "Basin": reservoir.basin,
-                "Sub Basin Name": reservoir.sub_basin_name,
-                "Agency": reservoir.agency,
-                "FRL": reservoir.frl,
-                "Live Cap FRL": reservoir.live_cap_frl,
-            }
-        return {}
-
     def _repr_html_(self):
-        """ Display the HydroFrame in a neat, interactive way """
-        display_data = self._generate_display_data()
-        
-        html_str = "<h3>HydroFrame Overview</h3>"
-        
-        # Add state list
-        html_str += "<b>States:</b><br>"
-        for state in display_data["States"]:
-            html_str += f"<button onclick='expandItem(\"state_{state}\")'>{state}</button><br>"
-            html_str += f"<div id='state_{state}' style='display:none'>{state}</div>"
-        
-        # Add reservoirs
-        html_str += "<br><b>Reservoirs:</b><br>"
-        for reservoir in display_data["Reservoirs"]:
-            html_str += f"<button onclick='expandItem(\"reservoir_{reservoir}\")'>{reservoir}</button><br>"
-            html_str += f"<div id='reservoir_{reservoir}' style='display:none'>{self._expand_reservoir(reservoir)}</div>"
-        
-        # Inject JS for expanding and collapsing
-        html_str += """
-        <script>
-        function expandItem(id) {
-            var item = document.getElementById(id);
-            if (item.style.display === 'none') {
-                item.style.display = 'block';
-            } else {
-                item.style.display = 'none';
-            }
-        }
-        </script>
         """
+        Generate an interactive HTML representation of PyWRIS HydroFrame Jupyter.
+        """
+        html = "======================================================"
+        html += f"<h1 style = 'margin-bottom: 0; margin-top: 0;'>PyWRIS HydroFrame</h1>"
+        html += "======================================================"
         
-        return html_str
+        # States Section
+        if self.states:
+            html += f"<h3 style='margin-bottom: 0;'>States: ({len(self.states)})</h3>"
+            for state_name, state_obj in self.states.items():
+                html += f"""
+                <details>
+                    <summary>{state_name}</summary>
+                    {state_obj._repr_html_() if hasattr(state_obj, '_repr_html_') else '<p>No details available for this state.</p>'}
+                </details>
+                """
+        else:
+            pass
+        
+        # Reservoirs Section
+        if self.reservoirs:
+            html+="<br>-------------------------"
+            html += f"<br><h3 style='margin-top: 0; margin-bottom: 0;'>Reservoirs: ({len(self.reservoirs)})</h3>"
+            for res_name, res_obj in self.reservoirs.items():
+                html += f"""
+                <details>
+                    <summary>{res_name}</summary>
+                    {res_obj._repr_html_() if hasattr(res_obj, '_repr_html_') else '<p>No details available for this reservoir.</p>'}
+                </details>
+                
+            """
+        else:
+            pass        
+        
+        # Basins Section
+        if self.basins:
+            html += f"<h3>Basins ({len(self.basins)}):</h3>"
+            for basin_name, basin_obj in self.basins.items():
+                html += f"""
+                <details>
+                    <summary><strong>Basin: {basin_name}</strong></summary>
+                    {basin_obj._repr_html_() if hasattr(basin_obj, '_repr_html_') else '<p>No details available for this basin.</p>'}
+                </details>
+                """
+        else:
+            pass
+        
+        if not self.basins and not self.states:
+            html += "<p>HydroFrame is currently empty as it has not been initialised with states.<br> Please pass a list of states to HydroFrame.add_state() to populate.</p>"
+
+
+        return html
+
+  
+
 
 def filter(hf, on, by, range=None, values=None):
     # Validate input
@@ -115,7 +119,7 @@ def filter(hf, on, by, range=None, values=None):
         if values is not None:
             filtered_df = hf.reservoir_df[hf.reservoir_df[by] in values]
         else:
-            filtered_df = hf.reservoir_df[hf.reservoir_df[by] >= range[0]] & hf.reservoir_df[hf.reservoir_df[by] <= range[1]
+            filtered_df = hf.reservoir_df[hf.reservoir_df[by] >= range[0]] & hf.reservoir_df[hf.reservoir_df[by] <= range[1]]
     else:
         raise ValueError("Invalid filter on.")
         
